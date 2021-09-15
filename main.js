ab516e15de94958d774258448814a4186a33322a
rashel68 <rashel341068@gmail.com>
2021-09-15 15:32:40 +0600

    slider

---
+++ main.js
@@ -0,0 +1,35 @@
+const imgContainer = [
+    'images/pic-1.jpg',
+    'images/pic-2.jpg',
+    'images/pic-3.jpg',
+    'images/pic-4.jpg',
+    'images/pic-5.jpg',
+    'images/pic-6.jpg'
+];
+const imgCaption = [
+    'away-hill-fields-tree-lane-lonely',
+    'sunset-tree-water-silhouette',
+    'fantasy-light-mood-sky-beautiful',
+    'away-hill-fields-tree-lane-lonely',
+    'sunset-tree-water-silhouette',
+    'fantasy-light-mood-sky-beautiful'
+]
+
+let slideImage = document.getElementById('slider-image');
+let slideCaption = document.getElementById('slide-caption');
+
+let slideIndex = 0;
+setInterval(() => {
+    // console.log(slideIndex);
+    if (slideIndex >= imgContainer.length) {
+        slideIndex = 0;
+
+    }
+    let img = imgContainer[slideIndex];
+    let imgCap = imgCaption[slideIndex];
+    slideCaption.innerText = `${slideIndex + 1} | ${imgCap}`;
+    slideImage.setAttribute('src', img);
+    slideIndex++
+
+    // console.log(img);
+}, 1400)
